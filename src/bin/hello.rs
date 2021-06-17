afbc26201aef62a59e41ee6b2334287d32c7dae2
dajamante <aimaiga2@gmail.com>
2021-06-17 11:16:25 +0200

    asm::nop

--- src/bin/hello.rs
+++ src/bin/hello.rs
@@ -5,12 +5,13 @@ use sensing_nrf52 as _; // global logger + panicking-behavior + memory layout
 
 use nrf52840_hal::{
     self as hal,
-    gpio::{p0::Parts as P0Parts, Input, Level, Output, Pin, PullUp, PushPull},
+    gpio::{p0::Parts as P0Parts, Level},
     prelude::{InputPin, OutputPin},
     Timer,
 };
 
-use embedded_hal::{blocking::delay::DelayMs, blocking::delay::DelayUs};
+use cortex_m::asm;
+use embedded_hal::blocking::delay::{DelayMs, DelayUs};
 
 #[cortex_m_rt::entry]
 fn main() -> ! {
@@ -22,6 +23,7 @@ fn main() -> ! {
     let mut trig = pins.p0_03.into_push_pull_output(Level::High).degrade();
     let mut echo = pins.p0_04.into_pullup_input().degrade();
     let mut btn = pins.p0_11.into_pullup_input().degrade();
+    let mut btn2 = pins.p0_12.into_pullup_input().degrade();
 
     // here should be if btn.is_low() { // set trigger, check what is sent from the echo}
     // if not unwrap :
@@ -29,18 +31,25 @@ fn main() -> ! {
     // expected `bool`, found enum `core::result::Result`
     // note: expected type `bool`
     // found enum `core::result::Result<bool, void::Void>`rustc(E0308)
-    loop {
+    'loopy: loop {
         if btn.is_low().unwrap() {
-            timer.delay_ms(1000u32);
             trig.set_high().unwrap();
-            timer.delay_ms(10u32);
+            timer.delay_us(10u32);
             trig.set_low().unwrap();
             defmt::info!("button pushed");
+            // Start timer?
+            // timer.task_start();
             let mut time_a = timer.read();
             defmt::info!("{=u32}", time_a);
-            //while echo.is_high().unwrap() {}
+            for _i in 0..10000 {
+                asm::nop;
+            }
             let mut time_b = timer.read();
             defmt::info!("{=u32}", time_b - time_a);
+            timer.delay_ms(100u32);
+        }
+        if btn2.is_low().unwrap() {
+            break 'loopy;
         }
     }
     sensing_nrf52::exit()
