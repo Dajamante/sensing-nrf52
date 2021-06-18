34df1932f9a808b6bdc8a37dac166b5db2f7a64d
dajamante <aimaiga2@gmail.com>
2021-06-18 16:47:24 +0200

    the loop freezes

--- src/bin/hello.rs
+++ src/bin/hello.rs
@@ -3,54 +3,59 @@
 
 use sensing_nrf52 as _; // global logger + panicking-behavior + memory layout
 
+// this is for nop if I need it
+
+use embedded_hal::blocking::delay::{DelayMs, DelayUs};
 use nrf52840_hal::{
     self as hal,
     gpio::{p0::Parts as P0Parts, Level},
-    prelude::{InputPin, OutputPin},
+    prelude::*,
     Timer,
 };
 
-use cortex_m::asm;
-use embedded_hal::blocking::delay::{DelayMs, DelayUs};
-
 #[cortex_m_rt::entry]
 fn main() -> ! {
-    defmt::info!("Hello, world!");
     let board = hal::pac::Peripherals::take().unwrap();
     let pins = P0Parts::new(board.P0);
-    let mut timer = Timer::new(board.TIMER0);
+    let mut timer_0 = Timer::new(board.TIMER0);
+    let mut timer_1 = Timer::new(board.TIMER1).into_periodic();
     // why no degrade on leds for example? And degrade on buttons?
-    let mut trig = pins.p0_03.into_push_pull_output(Level::High).degrade();
-    let mut echo = pins.p0_04.into_pullup_input().degrade();
-    let mut btn = pins.p0_11.into_pullup_input().degrade();
-    let mut btn2 = pins.p0_12.into_pullup_input().degrade();
+    let mut trig = pins.p0_03.into_push_pull_output(Level::Low).degrade();
+    let mut echo = pins.p0_04.into_pulldown_input().degrade();
 
-    // here should be if btn.is_low() { // set trigger, check what is sent from the echo}
-    // if not unwrap :
-    // mismatched types
-    // expected `bool`, found enum `core::result::Result`
-    // note: expected type `bool`
-    // found enum `core::result::Result<bool, void::Void>`rustc(E0308)
-    'loopy: loop {
-        if btn.is_low().unwrap() {
-            trig.set_high().unwrap();
-            timer.delay_us(10u32);
-            trig.set_low().unwrap();
-            defmt::info!("button pushed");
-            // Start timer?
-            // timer.task_start();
-            let mut time_a = timer.read();
-            defmt::info!("{=u32}", time_a);
-            for _i in 0..10000 {
-                asm::nop;
+    let mut millis: u32 = 0;
+    // the bigger loop
+    loop {
+        'small: loop {
+            // one second timer
+            timer_1.start(1000u32);
+
+            // send wave every 100 ms
+            if millis % 10 == 0 {
+                trig.set_high().unwrap();
+                timer_0.delay_us(10u32);
+                trig.set_low().unwrap();
             }
-            let mut time_b = timer.read();
-            defmt::info!("{=u32}", time_b - time_a);
-            timer.delay_ms(100u32);
-        }
-        if btn2.is_low().unwrap() {
-            break 'loopy;
+
+            // if 1 second passed without signal we waited too long
+            while echo.is_low().unwrap() {
+                if millis % 1000 == 0 {
+                    break 'small;
+                }
+            }
+
+            // // wait as long as the echo is high
+            // this is blocking the program!
+            while echo.is_high().unwrap() {}
+
+            defmt::info!("{=u32} : timer value", timer_1.read());
+
+            millis += 1;
+            defmt::info!("{=u32} passed millis", millis);
+
+            // blocking wait with timer_0 to not debounce the button
+            timer_0.delay_ms(100u32);
         }
     }
-    sensing_nrf52::exit()
+    //sensing_nrf52::exit()
 }
