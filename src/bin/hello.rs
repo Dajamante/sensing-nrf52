94dc44c7d46ea34e7c263e0f531718c3766de451
dajamante <aimaiga2@gmail.com>
2021-06-21 10:55:57 +0200

    completely wrong yet successful communication with sensor

--- src/bin/hello.rs
+++ src/bin/hello.rs
@@ -19,42 +19,48 @@ fn main() -> ! {
     let pins = P0Parts::new(board.P0);
     let mut timer_0 = Timer::new(board.TIMER0);
     let mut timer_1 = Timer::new(board.TIMER1).into_periodic();
-    // why no degrade on leds for example? And degrade on buttons?
     let mut trig = pins.p0_03.into_push_pull_output(Level::Low).degrade();
     let mut echo = pins.p0_04.into_pulldown_input().degrade();
 
-    let mut millis: u32 = 0;
     // the bigger loop
     loop {
         'small: loop {
-            // one second timer
-            timer_1.start(1000u32);
-
+            timer_1.start(1000000u32);
             // send wave every 100 ms
-            if millis % 10 == 0 {
+            if timer_1.read() % 1000 == 0 {
                 trig.set_high().unwrap();
                 timer_0.delay_us(10u32);
                 trig.set_low().unwrap();
+                defmt::info!("Sent wave");
             }
 
             // if 1 second passed without signal we waited too long
             while echo.is_low().unwrap() {
-                if millis % 1000 == 0 {
+                defmt::info!("Echo is low");
+                if timer_1.read() % 1000 == 0 {
+                    defmt::info!("No signal received, starting over from echo low.");
                     break 'small;
                 }
             }
 
-            // // wait as long as the echo is high
-            // this is blocking the program!
-            while echo.is_high().unwrap() {}
+            let mut start: u32 = 0;
+            // Wait while the echo is high,
+            // same here, if 1 second has passed the wait is too long
+            while echo.is_high().unwrap() {
+                if timer_1.read() % 1000 == 0 {
+                    defmt::info!("No signal received, starting over from echo high.");
+                    break 'small;
+                }
+                start = timer_1.read();
+                defmt::info!("Echo is high. {=u32} : timer value", timer_1.read());
+            }
+            let mut length = timer_1.read() - start;
+            defmt::info!("{=u32} : value counter", length);
 
-            defmt::info!("{=u32} : timer value", timer_1.read());
+            defmt::info!("{=u32} : Estimated distance", length / 58);
 
-            millis += 1;
-            defmt::info!("{=u32} passed millis", millis);
-
-            // blocking wait with timer_0 to not debounce the button
-            timer_0.delay_ms(100u32);
+            // Blocking wait to allow human reading
+            timer_0.delay_ms(1000u32);
         }
     }
     //sensing_nrf52::exit()
